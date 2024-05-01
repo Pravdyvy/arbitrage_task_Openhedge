25381ebc062e4aadfa8041ed9b662e497f866c3a
Oleksandr Pravdyvyi <awxrvtb@gmail.com>
2024-05-01 12:01:16 +0300

    feat: async runtime added

--- arbitrage_bot/src/main.rs
+++ arbitrage_bot/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use arbitrage_bot::main_loop;
+
+fn main() -> Result<()> {
+    actix::System::with_tokio_rt(|| {
+        tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(num_cpus::get())
+            .enable_all()
+            .build()
+            .unwrap()
+    })
+    .block_on(main_loop())
 }
